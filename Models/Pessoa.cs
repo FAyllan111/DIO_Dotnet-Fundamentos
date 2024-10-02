826c8b8634d493d03fae4f463a9e07ceb95ebacf
Ayllan Alves <franciscoayllan@gmail.com>
2024-10-02 17:48:21 -0300

    btcamp: pt1

---
+++ Models/Pessoa.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_dotnet_bootcamp.Models
+{
+    public class Pessoa // nome de classe PascalCase
+    {
+        public string Nome { get; set; } 
+        public int Idade { get; set; }
+
+        public double Salario { get; set; }
+
+        public DateTime Data { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá,meu nome é {Nome} e tenho {Idade} anos e recebo R$ {Salario}");
+            
+        }
+    }
+}
+
+// nome de classe, propriedade e métodos(variaveis) PascalCase
+// nome de variavel camelCase
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using dio_dotnet_bootcamp.Models;
+
+Pessoa cliente = new Pessoa();
+
+cliente.Nome = "Jozé";
+cliente.Idade = 368;
+cliente.Salario = 2965854559.23215151519451;
+cliente.Apresentar();
+
+Pessoa pessoaCamelCase = new Pessoa();
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
