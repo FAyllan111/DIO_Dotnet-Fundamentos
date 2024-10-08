e4d73e28343621396def56b5cdb9d71ce12db971
Ayllan Alves <franciscoayllan@gmail.com>
2024-10-08 11:06:11 -0300

    final_fundamentos

--- obj/Debug/net8.0/dio-dotnet-bootcamp.AssemblyInfo.cs
+++ obj/Debug/net8.0/dio-dotnet-bootcamp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dio-dotnet-bootcamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3c8b84211abd4ecd6a5a040e8bb91aae18b21b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f393b6cdf07a29c24c7f89ba8ae30114008b91e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("dio-dotnet-bootcamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dio-dotnet-bootcamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
